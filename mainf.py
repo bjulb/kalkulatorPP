a4bb7bce55b2fa8490fa1f377a6340a0b435c174
bjulb <rzul.jan@gmail.com>
2024-09-08 18:38:22 +0200

    Initial commit, add mainf.py to repo

---
+++ mainf.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+driver.get("https://cennik.poczta-polska.pl/usluga,krajowy_pocztex.html")
+
+driver.implicitly_wait(2))
+
+driver.quit()
