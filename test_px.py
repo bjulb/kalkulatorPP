1dcfc6b02c64b7c4379b1236bdf6f1353237250e
bjulb <rzul.jan@gmail.com>
2024-11-10 23:17:43 +0100

    Add main test files and pytest configuration files

--- test_px.py
+++ test_px.py
@@ -3,6 +3,6 @@ from selenium import webdriver
 driver = webdriver.Firefox()
 driver.get("https://cennik.poczta-polska.pl/usluga,krajowy_pocztex.html")
 
-driver.implicitly_wait(2))
+driver.implicitly_wait(2)
 
 driver.quit()
